f3d56237dfdc197576c351bf4410ad0e6f536b70
mauu95 <mauu95@hotmail.it>
2020-04-18 14:37:09 +0200

    Generate Chunk based on player position

---
+++ KeepItAlive/Assets/Scripts/PCGMap.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCGMap : MonoBehaviour
+{
+    public int seed;
+    public Transform player;
+    public GameObject background;
+
+    private IteratorSeed iseed;
+    private int difficulty;
+    private int counter;
+    private GameObject map;
+
+    private void Start()
+    {
+        iseed = new IteratorSeed(seed);
+        map = CreateEmptyGameObject("map");
+        CreateChunk(0);
+    }
+
+    private void Update()
+    {
+        int current = Mathf.FloorToInt(player.position.x / 100);
+        if(current >= counter)
+        {
+            CreateChunk(++counter);
+        }
+    }
+
+    private void CreateChunk(int nChunk)
+    {
+        GameObject currentChunk = CreateEmptyGameObject("Chunk" + counter.ToString());
+        currentChunk.transform.SetParent(map.transform);
+
+        Instantiate(background, new Vector3(nChunk*100, 0f), Quaternion.identity ,currentChunk.transform);
+    }
+
+
+    private GameObject CreateEmptyGameObject(string name)
+    {
+        GameObject temp = new GameObject();
+        GameObject result = Instantiate(temp);
+        result.name = name;
+        Destroy(temp);
+        return result;
+    }
+}
