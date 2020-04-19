0bc51877a29961db751001f98f8e8d384200731e
Taranjit Singh <taran1995s@gmail.com>
2020-04-19 15:03:17 +0200

    Fire rotates with movement

---
+++ KeepItAlive/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+    public Rigidbody2D playerRB;
+
+    void Update() {
+        var angle = 90 + (playerRB.velocity.y * 2);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
