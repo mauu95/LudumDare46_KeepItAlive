d9f9aae484d30d06d5e44210fdf2b37e52a556f8
mauu95 <mauu95@hotmail.it>
2020-04-19 16:44:23 +0200

    Added explosion after getting pick ups

--- KeepItAlive/Assets/Scripts/PickableObject.cs
+++ KeepItAlive/Assets/Scripts/PickableObject.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class PickableObject : MonoBehaviour {
 
     public bool isGood = false;
+    public GameObject explosion;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isGood)
+            explosion.GetComponent<ParticleSystem>().startColor = Color.green;
+        else
+            explosion.GetComponent<ParticleSystem>().startColor = Color.red;
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
 }
